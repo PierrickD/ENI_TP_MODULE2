66e54bfad16cd9adbf4fd7b519d7189e74af5bce
unknown <pierrick.durrmeyer@gmail.com>
2020-03-18 09:20:06 +0100

    premier commit

---
+++ ConsoleApp2/Carre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Carre : Rectangle
+    {
+        public override int Largeur => Longueur;
+        public override string ToString()
+        {
+            return $"Carré de coté={Longueur}" + Environment.NewLine + base.ToString(true);
+        }
+    }
+}
